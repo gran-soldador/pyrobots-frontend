f53ef309b461ce0a1a8bdcff3de3fc9e17b843f3
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-10-08 23:14:33 -0300

    api connection

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import axios from "axios";
 
 function App() {
+  const [result, setResult] = useState(null);
+
+  const message = async () => {
+    try {
+      let res = await axios.get('http://127.0.0.1:8000/');
+      let result = res.data;
+      setResult(result);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  useEffect(() => {
+    message();
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>
+        {result}
+      </h1>
     </div>
   );
 }
