b29679609bcead40a9d16d428c2ad31ec35b3f0f
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-10-14 10:44:10 -0300

    PYR-41 changes in test

--- src/tests/CreatePartida.test.js
+++ src/tests/CreatePartida.test.js
@@ -1,122 +1,65 @@
 import React from 'react'
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import CreatePartida from '../components/CreatePartida'
 
 //Verifico que se encuentren los campos predeterminados
-test('Name Partida', () => {
+test('Estan los campos predeterminados?', () => {
   render(<CreatePartida/>);
   const namepartida = screen.getByPlaceholderText(/Ingrese el nombre de la partida/i);
-  expect(namepartida).toBeInTheDocument();
-})
-
-test('Cantidad de jugadores', () => {
-  render(<CreatePartida/>);
   const numjugadores = screen.getByPlaceholderText(/Ingrese la cantidad de jugadores/i);
-  expect(numjugadores).toBeInTheDocument();
-})
-
-test('Cantidad de juegos', () => {
-  render(<CreatePartida/>);
   const numjuegos = screen.getByPlaceholderText(/Ingrese la cantidad de juegos/i);
-  expect(numjuegos).toBeInTheDocument();
-})
-
-test('Cantidad de Rondas', () => {
-  render(<CreatePartida/>);
   const numrondas = screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i);
-  expect(numrondas).toBeInTheDocument();
-})
-
-test('Password', () => {
-  render(<CreatePartida/>);
   const password = screen.getByPlaceholderText(/Ingrese contraseña/i);
+  expect(namepartida).toBeInTheDocument();
+  expect(numjugadores).toBeInTheDocument();
+  expect(numjuegos).toBeInTheDocument();
+  expect(numrondas).toBeInTheDocument();
   expect(password).toBeInTheDocument();
 })
 
-test('Name Partida Vacio', () => {
+//
+test('Las entradas están vacias al inicio?', () => {
   render(<CreatePartida/>);
   const namepartida = screen.getByPlaceholderText(/Ingrese el nombre de la partida/i);
-  expect(namepartida).toBeInTheDocument('');
-})
-
-test('Cantidad de jugadores 1', () => {
-  render(<CreatePartida/>);
   const numjugadores = screen.getByPlaceholderText(/Ingrese la cantidad de jugadores/i);
-  expect(numjugadores).toBeInTheDocument(1);
-})
-
-test('Cantidad de juegos 0', () => {
-  render(<CreatePartida/>);
   const numjuegos = screen.getByPlaceholderText(/Ingrese la cantidad de juegos/i);
-  expect(numjuegos).toBeInTheDocument(0);
-})
-
-test('Cantidad de Rondas negativo', () => {
-  render(<CreatePartida/>);
   const numrondas = screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i);
-  expect(numrondas).toBeInTheDocument(-2);
-})
-
-test('Password vacio', () => {
-  render(<CreatePartida/>);
   const password = screen.getByPlaceholderText(/Ingrese contraseña/i);
+  expect(namepartida).toBeInTheDocument('hola');
+  expect(numjugadores).toBeInTheDocument(2);
+  expect(numjuegos).toBeInTheDocument(10);
+  expect(numrondas).toBeInTheDocument(999);
   expect(password).toBeInTheDocument('');
 })
 
-test('Cantidad de Rondas > 10000', () => {
-  render(<CreatePartida/>);
-  const numrondas = screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i);
-  expect(numrondas).toBeInTheDocument(1000000);
-})
-
-// cambian?
-test('Name Partida change', () => {
+// cambian los parámetros inválidos a válidos
+test('Si las entradas estan llenas en boton de crear tiene que estar activo', () => {
   render(<CreatePartida/>);
   const namepartida = screen.getByPlaceholderText(/Ingrese el nombre de la partida/i);
-  const testValue = 'prueba';
-  fireEvent.change(namepartida, { target: { value: testValue } });
-  expect(namepartida).toBeInTheDocument(testValue);
-})
-
-test('Cantidad de jugadores change', () => {
-  render(<CreatePartida/>);
   const numjugadores = screen.getByPlaceholderText(/Ingrese la cantidad de jugadores/i);
-  const testValue = 5;
-  fireEvent.change(numjugadores, { target: { value: testValue } });
-  expect(numjugadores).toBeInTheDocument(testValue);
-})
-
-test('Cantidad de juegos change', () => {
-  render(<CreatePartida/>);
   const numjuegos = screen.getByPlaceholderText(/Ingrese la cantidad de juegos/i);
-  const testValue = 5;
-  fireEvent.change(numjuegos, { target: { value: testValue } });
-  expect(numjuegos).toBeInTheDocument(testValue);
-})
-
-test('Cantidad de Rondas change', () => {
-  render(<CreatePartida/>);
   const numrondas = screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i);
-  const testValue = 10;
-  fireEvent.change(numrondas, { target: { value: testValue } });
-  expect(numrondas).toBeInTheDocument(testValue);
-})
-
-test('Password change', () => {
-  render(<CreatePartida/>);
   const password = screen.getByPlaceholderText(/Ingrese contraseña/i);
-  const testValue = 'prueba';
-  fireEvent.change(password, { target: { value: testValue } });
-  expect(password).toBeInTheDocument(testValue);
+  const buttonEl = screen.getByText('Crear');
+  const testValue1 = 'pr6';
+  const testValue2 = 5;
+  const testValue3 = 5;
+  const testValue4 = 10;
+  const testValue5 = 'prueba';
+  fireEvent.change(namepartida, { target: { value: testValue1 } });
+  fireEvent.change(numjugadores, { target: { value: testValue2 } });
+  fireEvent.change(numjuegos, { target: { value: testValue3 } });
+  fireEvent.change(numrondas, { target: { value: testValue4 } });
+  fireEvent.change(password, { target: { value: testValue5} });
+  expect(namepartida).toBeInTheDocument(testValue1);
+  expect(numjugadores).toBeInTheDocument(testValue2);
+  expect(numjuegos).toBeInTheDocument(testValue3);
+  expect(numrondas).toBeInTheDocument(testValue4);
+  expect(password).toBeInTheDocument(testValue5);
+  expect(buttonEl).toBeDisabled();
 })
 
-test('el boton debe estar deshabilitado', () => {
-  render(<CreatePartida />);
-  const buttonEl = screen.getByText('Crear');
-  expect(buttonEl).toBeDisabled();
-});
-
-test('Crear envia datos', () => {
+test('Crear envia datos?', () => {
   render(<CreatePartida/>);
   const buttonis = screen.getByText('Crear');
   const namepartida = screen.getByPlaceholderText(/Ingrese el nombre de la partida/i);
@@ -140,4 +83,34 @@ test('Crear envia datos', () => {
   expect(numjuegos).toBeInTheDocument(testJug);
   expect(numrondas).toBeInTheDocument(testRound);
   expect(password).toBeInTheDocument(testPass);
+})
+
+test('Renderizar componentes async?', async () => { 
+  render(<CreatePartida/>)
+  const namepartida = screen.getByPlaceholderText(/Ingrese el nombre de la partida/i);
+  const numjugadores = screen.getByPlaceholderText(/Ingrese la cantidad de jugadores/i);
+  const numjuegos = screen.getByPlaceholderText(/Ingrese la cantidad de juegos/i);
+  const numrondas = screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i);
+  const password = screen.getByPlaceholderText(/Ingrese contraseña/i);
+
+  const buttonis = screen.getByText('Crear');
+
+  fireEvent.change(namepartida, { target: { value: 'partida' } });
+  fireEvent.change(numjugadores, { target: { value: 6 } });
+  fireEvent.change(numjuegos, { target: { value: 2 } });
+  fireEvent.change(numrondas, { target: { value: 25 } });
+  fireEvent.change(password, { target: { value: '123' } });
+
+  fireEvent.click(buttonis);
+  expect(namepartida).toBeInTheDocument();
+  expect(numjugadores).toBeInTheDocument();
+  expect(numjuegos).toBeInTheDocument();
+  expect(numrondas).toBeInTheDocument();
+  expect(password).toBeInTheDocument();
+
+  expect(screen.getByPlaceholderText(/Ingrese el nombre de la partida/i)).toHaveValue('partida');
+  expect(screen.getByPlaceholderText(/Ingrese la cantidad de jugadores/i)).toHaveValue(6);
+  expect(screen.getByPlaceholderText(/Ingrese la cantidad de juegos/i)).toHaveValue(2);
+  expect(screen.getByPlaceholderText(/Ingrese la cantidad de rondas/i)).toHaveValue(25);
+  expect(screen.getByPlaceholderText(/Ingrese contraseña/i)).toHaveValue('123');
 })
