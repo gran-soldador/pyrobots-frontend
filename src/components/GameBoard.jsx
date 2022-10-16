d0a808605d420c6c7189b9395376fb494e87d761
Federico Moreno <federico.moreno@mi.unc.edu.ar>
2022-10-16 18:47:17 -0300

    PYR-45 modificada la lectura de los datos del backend

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -11,7 +11,6 @@ import greenRobotImage from '../media/verde.svg';
 export function GameBoard() {
     
     const canvasRef = useRef();
-    const robotList = ["HK-47", "R2-D2", "MESSI", "C-3PO"];
     
     const [dataSimulation, setDataSimulation] = useState([]);
     const [isLoading, setLoading] = useState(true);
@@ -20,16 +19,16 @@ export function GameBoard() {
         
         const yellowRobot = new Image();
         yellowRobot.src = yellowRobotImage;
-        
+
         const redRobot = new Image();
         redRobot.src = redRobotImage;
         
         const blueRobot = new Image();
         blueRobot.src = blueRobotImage;
-        
+
         const greenRobot = new Image();
         greenRobot.src = greenRobotImage;
-        
+
         greenRobot.onload = () => {
             render()
         };
@@ -45,18 +44,30 @@ export function GameBoard() {
             
             ctx.clearRect(0, 0, width, height);
             
-            ctx.drawImage(yellowRobot, index, 50, robotSize, robotSize);
+            if (dataSimulation.robots[0].damage[index] < 100) {
+                ctx.drawImage(yellowRobot, dataSimulation.robots[0].positions[index].x, 
+                    dataSimulation.robots[0].positions[index].y, robotSize, robotSize);
+            }
+            if (dataSimulation.robotcount > 0) {
+                if (dataSimulation.robots[1].damage[index] < 100) {
+                    ctx.drawImage(redRobot, dataSimulation.robots[1].positions[index].x, 
+                        dataSimulation.robots[1].positions[index].y, robotSize, robotSize);
+                }
+            }
+            if (dataSimulation.robotcount > 1) {
+                if (dataSimulation.robots[2].damage[index] < 100) {
+                    ctx.drawImage(blueRobot, dataSimulation.robots[2].positions[index].x, 
+                        dataSimulation.robots[2].positions[index].y, robotSize, robotSize);
+                }
+            }
+            if (dataSimulation.robotcount > 2) {
+                if (dataSimulation.robots[3].damage[index] < 100) {
+                    ctx.drawImage(greenRobot, dataSimulation.robots[3].positions[index].x, 
+                        dataSimulation.robots[3].positions[index].y, robotSize, robotSize);
+                }
+            }
 
-            ctx.drawImage(redRobot, dataSimulation["B2"].positions[index].x, 
-                        dataSimulation["B2"].positions[index].y, robotSize, robotSize);
-
-            ctx.drawImage(blueRobot, dataSimulation["A1"].positions[index].x, 
-                        dataSimulation["A1"].positions[index].y, robotSize, robotSize);
-            
-
-            ctx.drawImage(greenRobot, 0, 700, robotSize, robotSize);
-            
-            if(index < dataSimulation["A1"].positions.length-1){
+            if(index < dataSimulation.rounds){
                 index++;
             }
             else{
@@ -75,7 +86,7 @@ export function GameBoard() {
             
             axios.get(URL)
             .then((response) => {
-                setDataSimulation(response.data.robots);
+                setDataSimulation(response.data);
                 setLoading(false);
             })
             .catch((e) => {
@@ -90,6 +101,7 @@ export function GameBoard() {
 
     function runAnimation (){
         if(!isLoading){
+            console.log(dataSimulation);
             drawingIncanvas(dataSimulation);
         }
         else{
@@ -108,22 +120,22 @@ export function GameBoard() {
                 <div className="container-progressbar">
                     <div className="row">
                         <div className="col-12 col-lg-5 offset-lg-1">
-                            <p>{robotList[0]}</p>
+                            <p>{/*dataSimulation.robots[0].name*/}</p>
                             <div className="progress mb-2" style={{height: '25px'}}>
                                 <div className="progress-bar azul-pbar " role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}>100%</div>
                             </div>
 
-                            <p>{robotList[1]}</p>
+                            <p>{/*dataSimulation.robots[1].name*/}</p>
                             <div className="progress mb-2" style={{height: '25px'}}>
                                 <div className="progress-bar verde-pbar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: '75%'}}>75%</div>
                             </div>
                         </div>
                         <div className="col-12 col-md-6 col-lg-5">
-                            <p>{robotList[2]}</p>
+                            <p>{/*dataSimulation.robots[2].name*/}</p>
                             <div className="progress mb-2" style={{height: '25px'}}>
                                 <div className="progress-bar amarillo-pbar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style={{width: '50%'}}>50%</div>
                             </div>
-                            <p>{robotList[3]}</p>
+                            <p>{/*dataSimulation.robots[3].name*/}</p>
                             <div className="progress mb-2" style={{height: '25px'}}>
                                 <div className="progress-bar rojo-pbar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}>25%</div>
                             </div>
