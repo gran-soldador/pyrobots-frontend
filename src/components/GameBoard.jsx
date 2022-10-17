23bbaea2cb5b24d0819671eb2409ee3187da88e0
Lautaro <lautir4mirez@gmail.com>
2022-10-16 23:00:58 -0300

    PYR-47 gameboard parameterized

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,5 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react'
 
+import './css/GameBoard.css';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from "axios";
 
@@ -11,40 +13,45 @@ import greenRobotImage from '../media/verde.svg';
 export function GameBoard() {
     
     const canvasRef = useRef();
-    
+
     const [dataSimulation, setDataSimulation] = useState([]);
     const [isLoading, setLoading] = useState(true);
-    
+
+    const [progressBar, setProgressBar] = useState("");
+    var index = 0;
+   
     const drawingIncanvas = (dataSimulation) => {
         
         const yellowRobot = new Image();
         yellowRobot.src = yellowRobotImage;
-
+        
         const redRobot = new Image();
         redRobot.src = redRobotImage;
         
         const blueRobot = new Image();
         blueRobot.src = blueRobotImage;
-
+        
         const greenRobot = new Image();
         greenRobot.src = greenRobotImage;
-
+        
         greenRobot.onload = () => {
             render()
         };
-        
-        var index = 0;
+
         const render = () => {
+            
             const canvas = canvasRef.current;
             const ctx = canvas.getContext("2d");
             
             const width = canvas.width;
             const height = canvas.height;
             const robotSize = 50;
-            const robotList = [yellowRobot, redRobot, blueRobot, greenRobot];
-            
-            ctx.clearRect(0, 0, width, height);
 
+            const robotList = [yellowRobot, redRobot, blueRobot, greenRobot];
+
+            setProgressBar(createDamageBar());
+            ctx.clearRect(0, 0, width, height);
+            
             for (let i = 0; i < dataSimulation.robotcount; i++) {
                 if (dataSimulation.robots[i].damage[index] < 100) {
                     ctx.drawImage(robotList[i], dataSimulation.robots[i].positions[index].x-robotSize/2, 
@@ -56,7 +63,6 @@ export function GameBoard() {
                 index++;
             }
             else{
-                console.log("Termine con index: ", index);
                 return;
             }
             
@@ -78,62 +84,70 @@ export function GameBoard() {
                 console.log(e);
             });
         }
-        
+
         getDataSimulation();
         
     }, []);
     
 
+    function createDamageBar(){
+
+        let renderElements = [];
+
+        let colorBars = ["amarillo-pbar", "rojo-pbar", "azul-pbar", "verde-pbar"];
+
+        for (let i = 0; i < dataSimulation.robotcount; i++) {
+            let currentDamage = 100 - dataSimulation.robots[i].damage[index];
+            
+            renderElements.push( 
+                <div className="col" key={"robot"+i}>
+                    <div className='robot-damage'>
+                        <p>{dataSimulation.robots[i].name}</p>
+                        <div className="progress mb-2" style={{height: '25px'}}>
+                            <div className={"progress-bar " + colorBars[i]}
+                                role="progressbar" 
+                                aria-valuenow="100" 
+                                aria-valuemin="0" 
+                                aria-valuemax="100" 
+                                style={{width: currentDamage.toString()+'%'}}> 
+                                    {currentDamage}% 
+                            </div>
+                        </div>
+                    </div> 
+                </div>
+            )
+        }
+        return(
+            renderElements
+        )
+    };
+    
     function runAnimation (){
         if(!isLoading){
-            console.log(dataSimulation);
-            drawingIncanvas(dataSimulation);
+            drawingIncanvas(dataSimulation);                
         }
         else{
             alert("Espera unos segundos e intente de nuevo.")
         }
     }
-        
+
     return (
         <div className='gameboard-pyrobots'>
-
                 <header className="mb-3">
                     <h1>Simulación de PyRobots</h1>
                     <hr />
                 </header>
 
-                <div className="container-progressbar">
-                    <div className="row">
-                        <div className="col-12 col-lg-5 offset-lg-1">
-                            <p>{/*dataSimulation.robots[0].name*/}</p>
-                            <div className="progress mb-2" style={{height: '25px'}}>
-                                <div className="progress-bar azul-pbar " role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100" style={{width: '100%'}}>100%</div>
-                            </div>
-
-                            <p>{/*dataSimulation.robots[1].name*/}</p>
-                            <div className="progress mb-2" style={{height: '25px'}}>
-                                <div className="progress-bar verde-pbar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style={{width: '75%'}}>75%</div>
-                            </div>
-                        </div>
-                        <div className="col-12 col-md-6 col-lg-5">
-                            <p>{/*dataSimulation.robots[2].name*/}</p>
-                            <div className="progress mb-2" style={{height: '25px'}}>
-                                <div className="progress-bar amarillo-pbar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style={{width: '50%'}}>50%</div>
-                            </div>
-                            <p>{/*dataSimulation.robots[3].name*/}</p>
-                            <div className="progress mb-2" style={{height: '25px'}}>
-                                <div className="progress-bar rojo-pbar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100" style={{width: '25%'}}>25%</div>
-                            </div>
-                        </div>
-                    </div>
+            <div className="container-progressbar">
+                <div className="row">
+                        {progressBar}
                 </div>
+            </div> 
 
-                <canvas id='canvas' width={1000} height={1000} ref={canvasRef}/>
-                <br></br>
-                <input id="clickMe" type="button" value="clickme" onClick={runAnimation} />
-
+            <canvas id='canvas' width={1000} height={1000} ref={canvasRef}/>
+            <br></br>
+            <button type="button" className="btn btn-success" onClick={runAnimation}>Simular</button>
         </div>
     );
 };
 
-// export default GameBoard;
