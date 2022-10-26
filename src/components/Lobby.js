97d34a359c1a99c1abdb2bad765f26f602fa43e2
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-10-26 17:57:45 -0300

    PYR-87 skeleton lobby

---
+++ src/components/Lobby.js
@@ -0,0 +1,11 @@
+import React, { useEffect, useState, useRef } from "react";
+
+
+const Lobby = () => {
+    const [listPlayers, setListPlayers] = useState([]);
+    return (
+        <>
+        </>
+    );
+}
+export default Lobby;
