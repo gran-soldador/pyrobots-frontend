0e8b0c199bbf62f2a00188c38d110757bc43aefd
Federico Moreno <federico.moreno@mi.unc.edu.ar>
2022-10-26 01:51:01 -0300

    PYR-89 primer commit

---
+++ src/components/SimulationForm.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useRef, useState } from 'react';
+import { Form, Image, Button, Modal } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.css';
+import logo from './logo.png';
+import axios from 'axios';
+import './css/CreatePartida.css';
+
+const CreateSim = () => {
+return (
+  <Form className='form_create_sim' onSubmit={handleSubmit}>
+    
+  </Form>
+  )
+}
+
+export default CreateSim;
