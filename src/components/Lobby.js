d40762ed415fe05c019003a0993bca90f8bb1ff8
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-10-27 20:53:47 -0300

    PYR-87 lobby desing

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -1,12 +1,38 @@
 import React, { useEffect, useState, useRef } from "react";
+import './css/Lobby.css';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from "react-router-dom";
 
 
 const Lobby = () => {
-    const [listPlayers, setListPlayers] = useState([]);
-    return (
-      <>
-        <h1>Hola soy el Lobby</h1>
-      </>
-    );
+
+  return (
+    <div>
+      <div className="Title">
+        <h1>Sala: #numero de la sala</h1>
+        <table className='table table-striped table-hover mt-5 shadow-lg'
+          cellSpacing='0'
+          cellPadding='0'
+          id='key-lobby'>
+        <thead className='table-dark'>
+            <tr>
+              <th>Nombre</th>
+              <th>Robot</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Nombre del jugador</td>
+              <td><button> Dropdown</button></td>
+            </tr>
+          </tbody>
+        </table>
+        <div className="GameButton-lobby">
+          <Button variant='primary'> Iniciar </Button> &nbsp;
+          <Button variant='secondary'> Salir </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default Lobby;
