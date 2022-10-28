1b2c69e74921d768553391fe1960516edf118003
Lautaro <lautir4mirez@gmail.com>
2022-10-28 19:48:34 -0300

    join game with ws

--- src/components/Lobby.js
+++ src/components/Lobby.js
@@ -3,15 +3,10 @@ import './css/Lobby.css';
 import { Button, Modal, Form } from 'react-bootstrap';
 import axios from 'axios';
 
-
 const Lobby = () => {
+
   const [listPlayers, setListPlayers] = useState([])
   const ws = useRef(null)
-
-  const handleCloseModal = () => {
-    setIsJoined(true)
-    setJoinGameForm(false);
-  }
 
   const [isReady, setIsready] = useState(false)
   const [isJoined, setIsJoined] = useState(false)
@@ -19,9 +14,43 @@ const Lobby = () => {
   const [datosRobot, setDatosRobot] = useState([]);
   const [idrobot, setIDRobot] = useState(0);
   const [joinGameForm, setJoinGameForm] = useState(false);
+  
+  const [isHost, setIsHost] = useState(false);
+
+  function handleCloseModal(event) {
+    event.preventDefault()
+    console.log(idrobot)
+    // Conectar Endpoint
+
+    const baseURL = "http://127.0.0.1:8000/unir-partida";
+    
+    const tokenDict = localStorage.getItem('user');
+    const tokenValue = (JSON.parse(tokenDict)).accessToken;
+
+    let formData = new FormData();
+    formData.append('partida_id', localStorage.getItem("partida_id"));
+    formData.append('id_robot', idrobot);
+
+    
+
+    axios.post(baseURL, formData, {
+      headers: {"Authorization" : `Bearer ${tokenValue}`}
+    })
+    .then((res) => {
+      console.log(res)
+    }) 
+    .catch((err) => {
+      console.log(err)
+    })
+
+    setIsJoined(true);
+    setJoinGameForm(false);
+  }
+
 
   //Leer datos de robots
   useEffect(function () {
+    console.log("lee datos robots")
     const tokenDict = localStorage.getItem('user');
     if (tokenDict !== null) {
       const tokenValue = (JSON.parse(tokenDict)).accessToken;
@@ -29,7 +58,7 @@ const Lobby = () => {
         headers: { 'Authorization': `Bearer ${tokenValue}` }
       })
       .then((res) => {
-        console.log(res)
+        // console.log(res)
         setDatosRobot(res.data)
       })
       .catch((err) => {
@@ -39,30 +68,64 @@ const Lobby = () => {
     }, []);
 
   useEffect(() => {
-    const partida_id = localStorage.getItem('id_lobby');
-    console.log("El id es: ", partida_id)
-    ws.current = new WebSocket('ws://localhost:8000/ws/' + partida_id)
+
+    console.log("El id es: ", localStorage.getItem('id_lobby'))
+    ws.current = new WebSocket('ws://localhost:8000/ws/' + localStorage.getItem('id_lobby'))
     ws.current.onmessage = (event) => {
-      console.log("recibiendo datos lobby");
+      console.log("data: ", event.data)
       setListPlayers(JSON.parse(event.data));
       setIsready(true);
+
+      if(JSON.parse(event.data).creador === localStorage.getItem("username")){
+        setIsHost(true);
+      }
+    
     };
+
+
   }, []);
 
-  if(isReady){
-    console.log(listPlayers);
+  
+  function joinGame (){
+
+    setJoinGameForm(true)
+
   }
 
-  function joinGame (){
-    setJoinGameForm(true)
-    console.log("enviar: ", idrobot)
-    console.log("Work here!")
+
+  function listLobby(){
+    let renderElements = [];
+    if(isReady){
+
+      console.log("lp", listPlayers)
+      
+      for (let index = 0; index < listPlayers.robot.length; index++) {
+        renderElements.push(
+          <tr key={index}>
+          <td> {listPlayers.robot[index].usuario} </td>
+          <td> {listPlayers.robot[index].nombre} </td>
+        </tr>
+      )
+    }
+  }
+    
+    return renderElements;
+
+  };
+
+  function boton_correcto(){
+    if(isHost){
+      return "Iniciar"
+    }
+    else{
+      return isJoined ? "Iniciar" : "Unirse"
+    } 
   }
   
   return (
     <div>
       <div className="Title">
-        <h1>Sala: #numero de la sala</h1>
+        <h1>Sala: {localStorage.getItem('id_lobby')}</h1>
         <table className='table table-striped table-hover mt-5 shadow-lg'
           cellSpacing='0'
           cellPadding='0'
@@ -74,15 +137,12 @@ const Lobby = () => {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>Nombre del jugador</td>
-              <td> Robot Elegido</td>
-            </tr>
+            {listLobby()}
           </tbody>
         </table>
         <div className="GameButton-lobby">
           <Button variant='primary' onClick={() => {joinGame()}}> 
-            { isJoined ? "Iniciar" : "Unirse" } 
+            { boton_correcto() } 
           </Button> &nbsp;
 
           <Button variant='secondary'> Salir </Button>
@@ -100,10 +160,12 @@ const Lobby = () => {
               </Modal.Header>
               <Modal.Body>
                 
-              <Form.Group className='form-group'>
+              <Form.Group className='form-group' onSubmit={handleCloseModal}>
+                
                 <Form.Label>
                   Seleccione un robot:
                 </Form.Label>
+
                 <Form.Control
                   multiple
                   type='select'
@@ -116,18 +178,19 @@ const Lobby = () => {
                     )
                     }
                 </Form.Control>
-              </Form.Group>
 
+                  <Modal.Footer>
+                      <Button 
+                          variant="primary" 
+                          onClick={handleCloseModal}
+                          className='buttonModal'>
+                          
+                              Aceptar
+                      </Button>
+                  </Modal.Footer>
 
+                  </Form.Group>
               </Modal.Body>
-              <Modal.Footer>
-                  <Button 
-                      variant="primary" 
-                      onClick={handleCloseModal}
-                      className='buttonModal'>
-                          Aceptar
-                  </Button>
-          </Modal.Footer>
       </Modal>
 
 
