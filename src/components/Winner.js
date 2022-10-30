59daddc211e8129d63a48114183e0799b3567fac
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-10-30 12:27:49 -0300

    PYR-79 desing changes

--- src/components/Winner.js
+++ src/components/Winner.js
@@ -35,41 +35,12 @@ const MainPage = () => {
     }
   }
 
-  // <div style="overflow:hidden; position:absolute; left:0; top:0; width:50px; height:25px;">
-  //   <div>
-  //     <h1> Ganador: </h1>
-  //   </div>
-  //     <table className='table-winner'>
-  //       <tbody>
-  //         {!isEmptyList && result.map((winner, id) => (
-  //           <tr key={ id }>
-  //           <td> Jugador: { winner.usuario },&nbsp;</td>
-  //           <td> Robot: { winner.robot } </td>
-  //          </tr>
-  //         ))}
-  //       </tbody>
-  //     </table>
-  // </div>
-  // <>
-  //   <div id="titles">
-  //     <div id="titlecontent">
-        
-  //       <p className="center">Ganador<br /></p>
-  //       {!isEmptyList && result.map((winner, id) => (
-  //         <p className="center" key={id}>
-  //           Jugador: {winner.usuario}, Robot: {winner.robot}
-  //         </p>
-  //       ))}
-  //     </div>
-  //   </div>
-  // </>
   return (
     <p>
       Ganador
       {!isEmptyList && result.map((winner, id) => (
         <span className="center" key={id}>
-          {winner.usuario},
-          Robot: {winner.robot}
+          {winner.usuario}
         </span>
         ))}
       &mdash; PyRobots &mdash;
