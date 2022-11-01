953661003eb5f159c2823d7d72a5e8a620aed5d7
veronica leaño <verito.cba933@gmail.com>
2022-11-01 14:03:08 -0300

    Uso de función async(), para que pase el archivo de test GameBoard.test.js

---
+++ src/tests/GameBoard.test.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import GameBoard from '../components/GameBoard.jsx';
+
+test('render del componente tablero', () => {
+  render(<GameBoard />);
+});
