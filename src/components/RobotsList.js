ec332460c8f1fb0b9dce9cfc775e495e9d1e1e79
Kevin Mansilla <kevin.mansilla@mi.unc.edu.ar>
2022-11-18 21:26:14 -0300

    PYR-104 solved warning

--- src/components/RobotsList.js
+++ src/components/RobotsList.js
@@ -66,7 +66,7 @@ const Cards = () => {
         const url = window.URL.createObjectURL(new Blob([res.data]));
         const link = document.createElement('a');
         link.setAttribute('href', url);
-        link.setAttribute('download', `${localStorage.getItem('name_robot')}` + '.py');
+        link.setAttribute('download', `${localStorage.getItem('name_robot')}.py`);
         document.body.appendChild(link);
         link.click();
         if (res.data.detail === 'El robot no existe') {
